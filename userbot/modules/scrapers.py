d8cc22b820d31afa2463c5d43238b8decb7498ca
mrismanaziz <mrismanaziz@gmail.com>
2021-08-06 08:28:48 +0000

    pylint: auto fixes
    Signed-off-by: mrismanaziz <mrismanaziz@gmail.com>

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -36,7 +36,7 @@ from googletrans import LANGUAGES, Translator
 from gtts import gTTS
 from gtts.lang import tts_langs
 from humanize import naturalsize
-from requests import exceptions, get, post
+from requests import get
 from search_engine_parser import YahooSearch as GoogleSearch
 from telethon.tl.types import DocumentAttributeAudio, MessageMediaPhoto
 from wikipedia import summary
