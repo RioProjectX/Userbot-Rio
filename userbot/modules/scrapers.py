0cdfd65439faf29b2456045fb323d465753d1f3b
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-08-09 14:55:08 +0700

    Update scrapers.py

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -37,9 +37,11 @@ from googletrans import LANGUAGES, Translator
 from gtts import gTTS
 from gtts.lang import tts_langs
 from humanize import naturalsize
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
 from requests import get
-from search_engine_parser import YahooSearch as GoogleSearch
-from telethon.tl.types import DocumentAttributeAudio, MessageMediaPhoto
+from search_engine_parser import GoogleSearch
+from telethon.tl.types import DocumentAttributeAudio, DocumentAttributeVideo, MessageMediaPhoto
 from wikipedia import summary
 from wikipedia.exceptions import DisambiguationError, PageError
 from youtube_dl import YoutubeDL
@@ -54,6 +56,8 @@ from youtube_dl.utils import (
     XAttrMetadataError,
 )
 from youtube_search import YoutubeSearch
+from userbot.modules.upload_download import get_video_thumb
+from userbot.utils.FastTelethon import upload_file
 
 from userbot import (
     BOTLOG,
@@ -108,13 +112,13 @@ async def ocr_space_file(
 async def setlang(prog):
     global CARBONLANG
     CARBONLANG = prog.pattern_match.group(1)
-    await prog.edit(f"Bahasa untuk carbon.now.sh mulai {CARBONLANG}")
+    await prog.edit(f"Language for carbon.now.sh set to {CARBONLANG}")
 
 
-@register(outgoing=True, pattern="^.carbon")
+@register(outgoing=True, pattern=r"^\.carbon")
 async def carbon_api(e):
     """A Wrapper for carbon.now.sh"""
-    await e.edit("`Processing..`")
+    await e.edit("**Processing...**")
     CARBON = "https://carbon.now.sh/?l={lang}&code={code}"
     global CARBONLANG
     textx = await e.get_reply_message()
@@ -124,52 +128,43 @@ async def carbon_api(e):
     elif textx:
         pcode = str(textx.message)  # Importing message to module
     code = quote_plus(pcode)  # Converting to urlencoded
-    await e.edit("`Processing..\n25%`")
-    if os.path.isfile("/root/userbot/.bin/carbon.png"):
-        os.remove("/root/userbot/.bin/carbon.png")
+    await e.edit("**Processing...\n25%**")
+    dl_path = "./.carbon/"
+    file_path = dl_path + "carbon.png"
+    if os.path.isfile(file_path):
+        os.remove(file_path)
     url = CARBON.format(code=code, lang=CARBONLANG)
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.binary_location = GOOGLE_CHROME_BIN
-    chrome_options.add_argument("--window-size=1920x1080")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-gpu")
-    prefs = {"download.default_directory": "/root/userbot/.bin"}
-    chrome_options.add_experimental_option("prefs", prefs)
-    driver = webdriver.Chrome(executable_path=CHROME_DRIVER, options=chrome_options)
+    driver = await chrome()
     driver.get(url)
-    await e.edit("`Processing..\n50%`")
-    download_path = "/root/userbot/.bin"
+    await e.edit("**Processing...\n50%**")
     driver.command_executor._commands["send_command"] = (
         "POST",
         "/session/$sessionId/chromium/send_command",
     )
     params = {
         "cmd": "Page.setDownloadBehavior",
-        "params": {"behavior": "allow", "downloadPath": download_path},
+        "params": {"behavior": "allow", "downloadPath": dl_path},
     }
     driver.execute("send_command", params)
-    driver.find_element_by_xpath("//button[contains(text(),'Export')]").click()
-    # driver.find_element_by_xpath("//button[contains(text(),'4x')]").click()
-    # driver.find_element_by_xpath("//button[contains(text(),'PNG')]").click()
-    await e.edit("`Processing..\n75%`")
+    driver.find_element_by_css_selector('[data-cy="quick-export-button"]').click()
+    await e.edit("**Processing...\n75%**")
     # Waiting for downloading
-    while not os.path.isfile("/root/userbot/.bin/carbon.png"):
+    while not os.path.isfile(file_path):
         await sleep(0.5)
-    await e.edit("`Processing..\n100%`")
-    file = "/root/userbot/.bin/carbon.png"
-    await e.edit("`Uploading..`")
+    await e.edit("**Processing...\n100%**")
+    await e.edit("**Uploading...**")
     await e.client.send_file(
         e.chat_id,
-        file,
-        caption="Made using [Carbon](https://carbon.now.sh/about/),\
-        \na project by [Dawn Labs](https://dawnlabs.io/)",
+        file_path,
+        caption=(
+            "Made using [Carbon](https://carbon.now.sh/about/),"
+            "\na project by [Dawn Labs](https://dawnlabs.io/)"
+        ),
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
 
-    os.remove("/root/userbot/.bin/carbon.png")
+    os.remove(file_path)
     driver.quit()
     # Removing carbon.png after uploading
     await e.delete()  # Deleting msg
