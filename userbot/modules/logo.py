00773915d84a1020d601ceaf25f35df5ce7180a5
Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
2021-08-15 04:01:30 +0700

    test

--- userbot/modules/logo.py
+++ userbot/modules/logo.py
@@ -1,8 +1,9 @@
 # 🍀 © @tofik_dn
 # ⚠️ Do not remove credits
 
+import asyncio
 from telethon.errors.rpcerrorlist import YouBlockedUserError
-from userbot import CMD_HELP
+from userbot import CMD_HELP, bot
 from userbot.events import register
 
 
@@ -14,23 +15,24 @@ async def _(event):
     if not text:
         await event.edit("`Give a name too!`")
     else:
-        await event.edit("`Processing`")
+        p = await event.edit("`Processing`")
     chat = "@tdtapibot"
-    async with event.client.conversation(chat) as conv:
+    async with bot.conversation(chat) as conv:
         try:
             msg_start = await conv.send_message(f"/logo {text}")
             r = await conv.get_response()
             details = await conv.get_response()
             logo = await conv.get_response()
             """ - don't spam notif - """
-            await event.client.send_read_acknowledge(conv.chat_id)
+            await bot.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
-            await event.edit(
+            await p.edit(
                 "**Error: Mohon Buka Blokir** @tdtapibot **Dan Coba Lagi!**"
             )
             return
-        await event.edit("`Done!`")
-        await event.client.send_file(event.chat_id, logo)
+        await asyncio.sleep(1)
+        await p.edit("`Done!`")
+        await bot.send_file(event.chat_id, logo)
         await event.client.delete_messages(
             conv.chat_id, [msg_start.id, r.id, details.id, logo.id]
         )
