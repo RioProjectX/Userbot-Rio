f7bd37e53e7cfa64c535e781677a62552bbce429
Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
2021-08-15 02:29:05 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -107,7 +107,7 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/mrismanaziz/Man-Userbot.git")
+    "https://github.com/tofikdn/Man-Userbot.git")
 UPSTREAM_REPO_BRANCH = os.environ.get(
     "UPSTREAM_REPO_BRANCH", "Man-Userbot")
 
