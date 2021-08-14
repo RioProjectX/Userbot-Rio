c4b5e7309c4d0a84b7151c8a358fd0d5894ba0fa
Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
2021-08-15 03:42:17 +0700

    Create logo.py
    test

---
+++ userbot/modules/logo.py
@@ -0,0 +1,47 @@
+# 🍀 © @tofik_dn
+# ⚠️ Do not remove credits
+
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from userbot import CMD_HELP
+from userbot.events import register
+
+
+@register(outgoing=True, pattern=r"^\.logo(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    text = event.pattern_match.group(1)
+    if not text:
+        await event.edit("`Give a name too!`")
+    else:
+        await event.edit("`Processing`")
+    chat = "@tdtapibot"
+    async with event.client.conversation(chat) as conv:
+        try:
+            msg_start = await conv.send_message(f"/logo {text}")
+            r = await conv.get_response()
+            details = await conv.get_response()
+            logo = await conv.get_response()
+            """ - don't spam notif - """
+            await event.client.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await event.edit(
+                "**Error: Mohon Buka Blokir** @tdtapibot **Dan Coba Lagi!**"
+            )
+            return
+        await event.edit("`Done!`")
+        await event.client.send_file(event.chat_id, logo)
+        await event.client.delete_messages(
+            conv.chat_id, [msg_start.id, r.id, details.id, logo.id]
+        )
+        await event.delete()
+
+
+CMD_HELP.update(
+    {
+        "logo": "**Plugin : **`logo`\
+        \n\n  •  **Syntax :** `.logo` <text>\
+        \n  •  **Function : **Membuat logo dari Teks yang diberikan\
+    "
+    }
+)
