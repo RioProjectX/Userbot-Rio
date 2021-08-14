871a484f7cd15848ede46e87b8e4dfec2dfbbedb
Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
2021-08-15 04:22:54 +0700

    Update logo.py

--- userbot/modules/logo.py
+++ userbot/modules/logo.py
@@ -15,14 +15,12 @@ async def _(event):
     if not text:
         await event.edit("`Give a name too!`")
     else:
-        p = await event.edit("`Processing`")
+        await event.edit("`Processing`")
     chat = "@tdtapibot"
     async with bot.conversation(chat) as conv:
         try:
-            msg_start = await conv.send_message(f"/logo {text}")
-            r = await conv.get_response()
-            details = await conv.get_response()
-            logo = await conv.get_response()
+            msg = await conv.send_message(f"/logo {text}")
+            response = await conv.get_response()
             """ - don't spam notif - """
             await bot.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
@@ -30,11 +28,10 @@ async def _(event):
                 "**Error: Mohon Buka Blokir** @tdtapibot **Dan Coba Lagi!**"
             )
             return
-        await asyncio.sleep(1)
-        await p.edit("`Done!`")
-        await bot.send_file(event.chat_id, logo)
+        await asyncio.sleep(0.5)
+        await bot.send_file(event.chat_id, response)
         await event.client.delete_messages(
-            conv.chat_id, [msg_start.id, r.id, details.id, logo.id]
+            conv.chat_id, [msg.id, response.id]
         )
         await event.delete()
 
