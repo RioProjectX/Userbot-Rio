8327c5cf6eb4edd240ebd0694529afb4f78487ac
Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
2021-08-16 10:04:40 +0700

    Update logo.py

--- userbot/modules/logo.py
+++ userbot/modules/logo.py
@@ -33,7 +33,7 @@ async def _(event):
         await event.client.send_file(
             event.chat_id,
             logo,
-            caption=f"Logo by [{ALIVE_NAME}](tg://user?id={uid})"
+            caption=f"Logo by {ALIVE_NAME}"
         )
         await event.client.delete_messages(
             conv.chat_id, [msg.id, response.id, logo.id]
